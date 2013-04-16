b632b0afe57750b70111f81f2347f2aa2d32c11f
dave kim <innerb@gmail.com>
2013-04-16 13:27:05 -0700

    fixing otsway backspace. added test-logo

--- app/assets/javascripts/otsway.js
+++ app/assets/javascripts/otsway.js
@@ -43,77 +43,111 @@ $.fn.ot = function(){
 	var line_height = 28;
 	var container_width = 300, container_height = 240;
 	var frame_rate = 10;
-	var lerp_amt = 0;
+	var lerp_amt = 1; // 0 (animating) to 1 (not animating)
+	var target_top = 0, cur_top = 0;
+	var cur_code;
+	var steps = [];
 	var p = new Processing(this[0]);
 	var pp; // off-screen buffer
-	var letter = function(c){
-		this.c = c;
-		this.next_line_number = 0.25;
-		this.draw = function(){
-			var animated_line_number = pp.lerp(this.next_line_number - 1, this.next_line_number, lerp_amt);
-			var x = l_width/2;
-			var y = container_height - animated_line_number * line_height;
-			var z = - animated_line_number * line_height/2;
-			pp.text(this.c + (this.c == " " ? "" : " - ") + my_text[this.c], x, y, z);
-			//p.text(this.c, x, y, z);
-		}
-		return this;
-	};
 	p.setup = function() {
 		p.size(container_width, container_height, p.P3D);
+		
+		// off screen buffer
 		pp = p.createGraphics(container_width, container_height, p.P3D);
 		pp.beginDraw();
 		pp.stroke(0);
+		// set text height and preserve m-width
 		pp.textSize(l_height);
 		l_width = pp.textWidth('m');
 		pp.endDraw();
+		
 		p.loop();
 	}
-	p.draw = function(){
-		if(lerp_amt < 1){
-			lerp_amt += 0.1;
-		}
+	function direction(){
+		return target_top === cur_top ? 0 : target_top > cur_top ? 1 : -1
+	}
+	function is_backspace(code){
+		return code === 8 || code === 127
+	}
+	function my_animate(lerp_amt){
 		pp.beginDraw();
 		pp.background(255);
 		pp.noFill();
 		pp.rect(0,0,container_width, container_height);
 		pp.fill(64);
 		$.each(l, function(i, v){
-			v.draw();
-		})
+			var x = l_width/2;
+			var line = cur_top - i;
+			var line_lerp = direction() * pp.lerp(0, 1, lerp_amt);
+			var y = container_height - (line + line_lerp - 0.5) * line_height;
+			var z = -(line + line_lerp - 0.5) * line_height/2;
+			pp.text(v + ' - ' + my_text[v], x, y, z);
+		})	
 		pp.endDraw();
 		p.image(pp.get(), 0, 0);
-	}		
-	function handle_char_code(c){
-		if(c in my_text){
-			$.each(l, function(i, v){
-				v.next_line_number++;
-			})
-			l.push(new letter(c));
-			if(l.length > 12){
-				l.shift();
-			}
-			lerp_amt = 0;
-		}		
 	}
+	p.draw = function(){
+		if(lerp_amt < 1){ // animating
+			lerp_amt += 0.2;
+			my_animate(lerp_amt);
+		} else { // animation done
+			if(direction() === 0){
+			} else { // done animating
+				var old_direction = direction();
+				cur_top += old_direction;
+				cur_code = null;
+				if(old_direction < 0){ // backspacing
+					l.pop();
+					my_animate(0);
+				} else { // going forward
+				}
+			}
+			if(cur_code){
+				if(is_backspace(cur_code)){
+					if(target_top){
+						target_top--;
+						lerp_amt = 0;
+					} else {
+						cur_code = null; // just toss it
+					}
+				} else { // not backspace
+					var c = String.fromCharCode(cur_code);
+					if(c in my_text){
+						l.push(c);
+						target_top++;
+						lerp_amt = 0;
+					} else { // not in list
+						cur_code = null; // just toss it
+					}
+				}
+			} else { // no cur_code
+				if(steps.length){
+					cur_code = steps.shift();
+				} else {
+					// nothing to do
+				}
+			}
+		}
+	}		
 	this.keypress(function(e){
 		function get_char_code(){
 			if (e.which == null) 
-			return String.fromCharCode(e.keyCode);    // old IE
+			return e.keyCode;    // old IE
 			else if (e.which != 0 && e.charCode != 0)
-			return String.fromCharCode(e.which);	  // All others
+			return e.which;	  // All others
 			else {
 				debugger // special key
 				return false;
 			}			
 		}
-		var c = get_char_code();
-		c && handle_char_code(c);
+		var char_code = get_char_code();
+		steps.push(char_code);
 	})
 
 	p.setup();
 	$.each("hello world".split(""), function(i, v){
-		handle_char_code(v);
+		debugger
+		steps.push(v.charCodeAt());
 	})
 	return this;
 }
